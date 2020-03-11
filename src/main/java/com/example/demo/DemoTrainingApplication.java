b7b4ba78ed78fd6f1ae7909e7b5ac3759004692e
sumit <sumit@172.21.230.100>
2020-03-11 15:03:11 +0530

    commit

---
+++ src/main/java/com/example/demo/DemoTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTrainingApplication.class, args);
+	}
+
+}
